a0f4f5d0ef41b21b46263e721f21332f354595e0
Alistair Young <alistair@codebrane.com>
2010-05-03 15:57:59 +0100

    Updated method and member naming convention

--- src/util.cpp
+++ src/util.cpp
@@ -2,10 +2,15 @@
 #include <sstream>
 
 #include "util.h"
+#include "pod.h"
 
-string Util::generate_uuid() {
+string Util::generateUUID() {
 	srand(time(NULL));
 	stringstream out;
 	out << rand();
 	return out.str();
+}
+
+const char* Util::getCookieValue(request_rec* request, const char* cookie_name) {
+	 return apr_table_get(request->headers_in, cookie_name);
 }
