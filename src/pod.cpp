045584ff88f5582f9bcb429c74b62db7ca0593ae
Alistair Young <alistair@codebrane.com>
2010-05-03 15:57:22 +0100

    Updated method and member naming convention

--- src/pod.cpp
+++ src/pod.cpp
@@ -1,22 +1,22 @@
 #include "pod.h"
 
-Pod::Pod(string pod_session_id) : session_id(pod_session_id) {
+Pod::Pod(string podSessionID) : sessionID(podSessionID) {
 }
 
 Pod::~Pod() {
 }
 
-string Pod::get_session_id(void) {
-	return session_id.c_str();
+string Pod::getSessionID(void) {
+	return sessionID.c_str();
 }
 
-void Pod::set_saml_response(string saml_response) {
-	this->saml_response.assign(saml_response);
+void Pod::setSAMLResponse(string samlResponse) {
+	this->samlResponse.assign(samlResponse);
 }
 
-string Pod::get_saml_response(void) {
-	return saml_response.c_str();
+string Pod::getSAMLResponse(void) {
+	return sessionID.c_str();
 }
 
-void Pod::set_attributes(const char* json) {
+void Pod::setAttributes(const char* json) {
 }
