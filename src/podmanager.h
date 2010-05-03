183f9e0890dc81dfbf78f9a0df4130050f540a23
Alistair Young <alistair@codebrane.com>
2010-05-03 14:27:10 +0100

    PodManager definition

---
+++ src/podmanager.h
@@ -0,0 +1,45 @@
+#ifndef PODMANAGER_H_
+#define PODMANAGER_H_
+
+#include <vector>
+#include "pod.h"
+
+using namespace std;
+
+/// Class for managing Pods of attributes
+/**
+ * The PodManager is responsible for managing Pods of attributes and
+ * assertions, which are tied to cookie values. Once a user has
+ * authenticated at their IdP and we have all their attributes via
+ * the AttributeConsumerService, there will be a Pod tied to that
+ * cookie value.
+ *
+ * \author alistair
+ */
+class PodManager {
+public:
+	/// Default constructor
+	PodManager();
+
+	/// Default destructor
+	virtual ~PodManager();
+
+	/// Instructs the PodManager to add a new Pod tied to the specified session id
+	/**
+	 * \param session_id the cookie value which should be unique among the Pods
+	 */
+	void add_pod(string session_id);
+
+	/// Retrieves a Pod tied to the specified session id or NULL if it doesn't exist
+	/**
+	 * \param session_id the cookie value which should be unique among the Pods
+	 * \return the Pod for the session id or NULL if one doesn't exist
+	 */
+	Pod* get_pod(string session_id);
+
+private:
+	/// Internal list of Pods
+	vector<Pod*> pods;
+};
+
+#endif /* PODMANAGER_H_ */
