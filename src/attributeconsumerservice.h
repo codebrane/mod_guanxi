6fac2ce302cc04332a681ec16bb110ad82296a71
Alistair Young <alistair@codebrane.com>
2010-05-04 15:24:55 +0100

    Attribute Consumer Service definition

---
+++ src/attributeconsumerservice.h
@@ -0,0 +1,10 @@
+#ifndef ATTRIBUTECONSUMERSERVICE_H_
+#define ATTRIBUTECONSUMERSERVICE_H_
+
+class AttributeConsumerService {
+public:
+	AttributeConsumerService();
+	virtual ~AttributeConsumerService();
+};
+
+#endif /* ATTRIBUTECONSUMERSERVICE_H_ */
